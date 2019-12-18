d97e5beb1d79034e3b5c72d277c55f011e733e49
AARON M <asmarcks@gmail.com>
2019-12-18 00:02:14 -0600

    wrong repo whoops

--- Source.cpp
+++ Source.cpp
@@ -3,52 +3,15 @@ using namespace std;
 
 #include <Windows.h>
 
+#include <iostream>
+using namespace std;
+
+#include <Windows.h>
+
 int nScreenWidth = 120;
-int nScreenHeight = 40;
-
-float fPlayerX = 0.0f;
-float fPlayerY = 0.0f;
-float fPlayerA = 0.0f;
-
-int nMapHeight = 16;
-int nMapWidth = 16;
 
 int main() {
-	//create screen buffer//
-    wchar_t *screen = new wchar_t[nScreenWidth*nScreenHeight];
-    HANDLE hConsole = CreateConsoleScreenBuffer(GENERIC_READ | GENERIC_WRITE, 0, NULL, CONSOLE_TEXTMODE_BUFFER, NULL);
-    SetConsoleActiveScreenBuffer(hConsole);
-    DWORD dwBytesWritten = 0;
-
-    wstring map;
-
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-    map += L"################";
-
-    //game loop//
-    while (1)
-    {
-        for(int x=0; x < nScreenWidth; x++)
-        {
-            
-        }
-
-    screen[nScreenWidth * nScreenHeight - 1] = '\0';
-    WriteConsoleOutputCharacter(hConsole, screen, nScreenWidth * nScreenHeight, { 0,0 }, &dwBytesWritten);
-    }
+	for (int i = 0; i < 1000000; i++)
+		cout << "I am Satan!" << i << endl;
 	return 0;
 }
