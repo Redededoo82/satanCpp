13496b761c6105c518295df3a457b53f52afd532
AARON M <asmarcks@gmail.com>
2019-12-17 23:29:49 -0600

    the evil console app

---
+++ Source.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+#include <Windows.h>
+
+int nScreenWidth = 120;
+
+int main() {
+	for (int i = 0; i < 1000000; i++)
+		cout << "I am Satan!" << i << endl;
+	return 0;
+}
